d98835f513c28ea67c053e8b9c4dc913f33ae963
worong <hello@woicon.com>
2019-05-14 10:43:34 +0800

    hot

--- js/app.js
+++ js/app.js
@@ -1,24 +1,24 @@
 var app = {
 	tab:function(){
   $('.tab>div').on('click',function(e){
-    $(this).addClass('active').siblings().removeClass('active')
+    $(this).addClass('active').siblings().removeClass('active');
+    $(this).parent().siblings('.tabcont').eq($(this).index()).show().siblings().hide()
   })
 },
  tabBar:function(){
 	  let tabBar=`  <div class="tabbar">
-    <a href=".././home/home.html" exact activeclass="current">
-      <div class="icon icon-Home"></div>
-      首页
-    </a>
-    <a href="../../order/order.html" activeclass="current">
-      <div class="icon icon-Document"></div>
-      订单
-    </a>
-    <a href="../../member/member.html" activeclass="current">
-      <div class="icon icon-User" ></div>我
-    </a>
-  </div>`
+		  <a href="../home/home.html" exact activeclass="current">
+		    <div class="icon icon-Home"></div>
+		    首页
+		  </a>
+		  <a href="../order/order.html" activeclass="current">
+		    <div class="icon icon-Document"></div>
+		    订单
+		  </a>
+		  <a href="../member/member.html" activeclass="current">
+		    <div class="icon icon-User" ></div>我
+		  </a>
+		</div>`
 		$("body").append(tabBar);
-		console.log('ss')
   }
 }
